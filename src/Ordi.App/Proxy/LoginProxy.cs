7c870c0d125be6c6f9cfccd8c7134f9a4f68f52d
Mortal <mortal.wei@hotmail.com>
2019-03-15 19:00:21 +0800

    periodic submit

---
+++ src/Ordi.App/Proxy/LoginProxy.cs
@@ -0,0 +1,29 @@
+﻿using Ordi.IBusiness;
+using Ordi.Proxy;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Ordi.App.Proxy
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LoginProxy : WcfProxy<ILoginBusiness>
+    {
+        public LoginProxy()
+        {
+        }
+
+        protected LoginProxy(string hostAddress) : base(hostAddress)
+        {
+        }
+
+        public DataSet GetData()
+        {
+
+        }
+    }
+}
